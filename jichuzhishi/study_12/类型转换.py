70a368a47a3adf67ac2d28945e7a8a0370980095
luoxin <1260813538@qq.com>
2025-02-19 17:38:56 +0800

    一些修改

--- jichuzhishi/study_12/类型转换.py
+++ jichuzhishi/study_12/类型转换.py
@@ -1,8 +1,8 @@
 # @Time : 2023/11/16 22:45
 # @Author : luoxin
+from jichuzhishi.study_07.list_demo1 import names
 
-
-# str ---> int、list、set、tuple
+# str ---> int、list、set、tuple：除转换成int外，其余的都是将字符串拆开放入对应的容器
 
 s = '8'
 i = int(s)
@@ -19,8 +19,25 @@ print(t)  # {'f', 'e', 'd'}
 t = tuple(s)
 print(t)  # ('d', 'e', 'f')
 
-# int、list、set、tuple、dict、float ---> str
 
-# list ---> set、tuple，可以转成字典，但必须是以下格式[(key,vale),(key,vale),(key,vale)...]
+# int、list、set、tuple、dict、float ---> str：在最外层包裹一个引号
+print(type("[1,2,3]"))
+print(type("{1,2,3}"))
+print(type("(1,2,3)"))
+print(type("{'name':'jck','age':'18'}"))
+print(type("1.2"))
 
-# tuple ---> list    set ---> list   dict ---> lsit(只把key放入列表)
+# list ---> set、tuple，dict(可以转成字典，但必须是以下格式[(key,vale),(key,vale),(key,vale)...])
+print(set([1,2,3]))  # 等价于 {1,2,3}
+print(tuple([1,2,3])) # (1, 2, 3)
+print(dict([('name','jck'),('age','18')])) # {'name': 'jck', 'age': '18'}
+
+# tuple ---> list
+print(list((1,2,3))) # [1, 2, 3]
+
+# set ---> list
+print(list({1,2,3})) # [1,2,3]
+
+#dict ---> list(只把key放入列表)
+
+print(list({'name': 'jck', 'age': '18'})) # ['name','age']
